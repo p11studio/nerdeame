7b350846b8e3ced93be8365d69b5489d548264dd
Hugo Ponce <p11studyo@gmail.com>
2021-04-26 20:21:48 -0500

    Initial Commit

---
+++ Shared/nerdeameApp.swift
@@ -0,0 +1,17 @@
+//
+//  nerdeameApp.swift
+//  Shared
+//
+//  Created by Hugo Ponce on 26/04/21.
+//
+
+import SwiftUI
+
+@main
+struct nerdeameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
